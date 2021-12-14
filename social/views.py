f7a6a3c0d81d7ea94f46ed73fd41396e0340a5a9
Rengin <rengineker@gmail.com>
2021-12-14 19:17:13 +0300

    init

--- social/views.py
+++ social/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
-from .models import Post
+from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.views.generic.edit import UpdateView, DeleteView
 
@@ -35,19 +35,48 @@ class PostDetailView(View):
             def get(self, request, pk, *args, **kwargs):
                 post = Post.objects.get(pk=pk)
                 form= CommentForm()
+            
+                comments = Comment.objects.filter(post=post).order_by('-created_on')
+                
+                context = {
+                    'post' : post,
+                    'form' : form,
+                    'comments' : comments,
+                } 
+                
+                return render(request, 'social/post_detail.html', context)
+            
+            def post(self, request, pk, *args, **kwargs):
+                post = Post.objects.get(pk=pk)
+                form= CommentForm(request.POST)
+
+                if form.is_valid():
+                    new_comment = form.save(commit=False)
+                    new_comment.author = request.user
+                    new_comment.post = post
+                    new_comment.save()
+                
+                comments = Comment.objects.filter(post=post).order_by('-created_on')
 
                 context = {
                     'post' : post,
                     'form' : form,
+                    'comments' : comments,
                 } 
                 
                 return render(request, 'social/post_detail.html', context)
 
+
 class PostEditView(UpdateView):
-    model = Post, 
+    model = Post
     fields = ['body']
     template_name = 'social/post_edit.html'
 
     def get_success_url(self):
         pk = self.kwargs['pk']
         return reverse_lazy('post-detail', kwargs = {'pk': pk})
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('post-list')

89147faa63a3274e05aedf9374b4164e427b1a48
Rengin <rengineker@gmail.com>
2021-12-27 19:34:47 +0300

    commit

--- social/views.py
+++ social/views.py
@@ -80,3 +80,11 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'social/post_delete.html'
     success_url = reverse_lazy('post-list')
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    template_name = 'social/comment_delete.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs = {'pk': pk})

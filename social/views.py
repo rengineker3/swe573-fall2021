6004e7aa275db20f142b22f65ea29f4be4dcd11a
Rengin <rengineker@gmail.com>
2022-01-02 22:10:45 +0300

    commit

--- social/views.py
+++ social/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.views import View
@@ -30,7 +30,7 @@ class PostListView(LoginRequiredMixin, View):
                 'post_list':posts,
                 'form': form,
             }
-        return render(request, 'social/post_list.html', context)
+        return render(request, 'social/post_list.html', context)    
 
 class PostDetailView(LoginRequiredMixin, View):
             def get(self, request, pk, *args, **kwargs):
@@ -79,7 +79,7 @@ class PostEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author
+        return self.request.user == post.author        
 
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
@@ -90,6 +90,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
+       
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
@@ -107,13 +108,56 @@ class ProfileView(View):
     def get(self, request, pk, *args, **kwargs):
         profile = UserProfile.objects.get(pk=pk)
         user = profile.user
-        posts = Post.objects.filter(author=user).order_by('-created_on')  
+        posts = Post.objects.filter(author=user).order_by('-created_on') 
+
+        followers = profile.followers.all()
+
+        if len(followers) == 0:
+            is_following = False
+
+        for follower in followers:
+            if follower == request.user:
+                is_following = True
+                break
+            else:
+                is_following = False
+
+        number_of_followers = len(followers) 
 
         context = {
             'user' : user,
             'profile' : profile,
-            'posts' : posts
+            'posts' : posts,
+            'number_of_followers': number_of_followers,
+            'is_following' : is_following
 
         }     
 
         return render(request, 'social/profile.html', context)
+
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = UserProfile
+    fields = ['name', 'bio', 'birth_date', 'location', 'picture']
+    template_name = 'social/profile_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('profile', kwargs={'pk':pk})
+
+    def test_func(self):
+        profile = self.get_object()
+        return self.request.user == profile.user   
+
+class  AddFollower(LoginRequiredMixin, View):
+    def post( self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        profile.followers.add(request.user)
+
+        return redirect('profile', pk=profile.pk)
+
+class RemoveFollower(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        profile = UserProfile.objects.get(pk=pk)
+        profile.followers.remove(request.user)
+
+        return redirect('profile', pk=profile.pk)        
